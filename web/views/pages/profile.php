a0d163ac4da4178467f9be26e4a936779c9269e7
Memet Zx <zxce3@gnuweeb.org>
2023-04-19 05:37:25 +0700

    gwspamd: web: Shirnk the table to 40rem
    In the previous view the table is too wide. Shrink it.
    
    Signed-off-by: Memet Zx <zxce3@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20230418223726.139701-1-zxce3@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- web/views/pages/profile.php
+++ web/views/pages/profile.php
@@ -3,35 +3,37 @@
 $opt["title"] = $u["full_name"];
 
 ?>
-<div class="mw-full text-center">
-	<img id="user-photo" class="img-fluid rounded-circle" src="<?= e($u["photo_path"]); ?>" alt="<?= e($u["full_name"]); ?> Images">
-	<div class="content">
-		<h1 class="font-weight-bolder"><?= $u["full_name"]; ?></h1>
-		<a href="settings.php?section=profile" class="btn btn-action m-5">Edit Profile</a>
+<div class="mw-full  text-center">
+	<img id="user-photo" class="img-fluid mt-5 rounded-circle" src="<?= e($u["photo_path"]); ?>" alt="<?= e($u["full_name"]); ?> Images">
 
-		<table class="table table-striped">
-			<tbody>
-				<tr>
-					<th>User ID</th>
-					<td><?= e($u["id"]); ?></td>
-				</tr>
-				<tr>
-					<th>First Name</th>
-					<td><?= e($u["first_name"]); ?></td>
-				</tr>
-				<tr>
-					<th>Last Name</th>
-					<td><?= e($u["last_name"]); ?></td>
-				</tr>
-				<tr>
-					<th>User Name</th>
-					<td><?= e($u["username"]); ?></td>
-				</tr>
-				<tr>
-					<th>Email</th>
-					<td><?= e($u["email"]); ?></td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
+	<h1 class="font-weight-bolder"><?= $u["full_name"]; ?></h1>
+	<a href="settings.php?section=profile" class="btn btn-action m-5">Edit Profile</a>
+</div>
+
+<div class="mw-full d-flex justify-content-center">
+	<table class="table w-400 table-striped">
+		<tbody>
+			<tr>
+				<th>User ID</th>
+				<td><?= e($u["id"]); ?></td>
+			</tr>
+			<tr>
+				<th>First Name</th>
+				<td><?= e($u["first_name"]); ?></td>
+			</tr>
+			<tr>
+				<th>Last Name</th>
+				<td><?= e($u["last_name"]); ?></td>
+			</tr>
+			<tr>
+				<th>User Name</th>
+				<td><?= e($u["username"]); ?></td>
+			</tr>
+			<tr>
+				<th>Email</th>
+				<td><?= e($u["email"]); ?></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
 </div>
