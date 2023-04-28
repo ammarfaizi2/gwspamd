8395b486a20c150f0349807b316d2b990d02a5cb
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2023-04-28 23:43:15 +0700

    gwspamd: web: Limit the pagination in the audit log page
    Don't show too many buttons if the number of pages is massive. Create a
    mechanism to limit the number of displayed page button. Thanks to AI for
    helping crafting the log.
    
    Link: https://t.me/GNUWeeb/742171
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- web/views/pages/audit_log.php
+++ web/views/pages/audit_log.php
@@ -18,15 +18,69 @@ $st = get_audit_log_st($u["id"], $limit, $offset);
 $nr_pages = get_audit_log_page_num($u["id"], $limit);
 
 ob_start();
+
+const NUM_OF_BUTTONS = 3;
+
+$num_of_buttons = min($nr_pages, NUM_OF_BUTTONS);
+$first_button = ceil(max(min($page - $num_of_buttons / 2, $nr_pages - $num_of_buttons + 1), 1));
+$last_button = $first_button + $num_of_buttons - 1;
+$show_prev_dots = ($first_button > 1);
+$show_next_dots = ($last_button < $nr_pages - 1);
 ?>
 
 <nav aria-label="Pagination Log">
 	<ul class="pagination text-center m-5">
-		<?php for ($i = 1; $i <= $nr_pages; $i++) : ?>
+		<?php if ($page > 1): ?>
+			<li class="page-item<?= ($page == $i) ? " active" : ""; ?>">
+				<a href="?page=<?= $page - 1; ?>" class="page-link">&lt;</a>
+			</li>
+		<?php endif; ?>
+
+		<?php if ($show_prev_dots): ?>
+			<li class="page-item">
+				<a href="?page=1" class="page-link">1</a>
+			</li>
+			<li class="page-item ellipsis"></li>
+		<?php endif; ?>
+
+		<?php for ($i = $first_button; $i <= $last_button; $i++) : ?>
 			<li class="page-item<?= ($page == $i) ? " active" : ""; ?>">
 				<a href="?page=<?= $i; ?>" class="page-link"><?= $i; ?></a>
 			</li>
 		<?php endfor; ?>
+
+		<?php if ($show_next_dots): ?>
+			<li class="page-item ellipsis"></li>
+			<li class="page-item">
+				<a href="?page=<?= $nr_pages; ?>" class="page-link"><?= $nr_pages; ?></a>
+			</li>
+		<?php endif; ?>
+
+		<?php if ($page != $nr_pages): ?>
+			<li class="page-item">
+				<a href="?page=<?= $page + 1; ?>" class="page-link">&gt;</a>
+			</li>
+		<?php endif; ?>
+
+
+		<!-- <?php if ($page != 1): ?>
+			<li class="page-item<?= ($page == $i) ? " active" : ""; ?>">
+				<a href="?page=<?= $page - 1; ?>" class="page-link">&lt;</a>
+			</li>
+		<?php endif; ?>
+		<?php if ($show_prev_dots): ?>
+			<li class="page-item ellipsis"></li>
+		<?php endif; ?>
+		<?php for ($i = $start_pg + 1; $i <= $end_pg; $i++) : ?>
+			<li class="page-item<?= ($page == $i) ? " active" : ""; ?>">
+				<a href="?page=<?= $i; ?>" class="page-link"><?= $i; ?></a>
+			</li>
+		<?php endfor; ?>
+		<?php if ($page != $nr_pages): ?>
+			<li class="page-item">
+				<a href="?page=<?= $page + 1; ?>" class="page-link">&gt;</a>
+			</li>
+		<?php endif; ?> -->
 	</ul>
 </nav>
 <?php
